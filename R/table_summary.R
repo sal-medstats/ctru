af336c1b62ed01625b8f36d425047c4023e49f39
Neil Shephard <n.shephard@sheffield.ac.uk>
2017-05-26 09:25:03 +0100

    Working version that uses Non-Standard Evaluation \o/

---
+++ R/table_summary.R
@@ -0,0 +1,48 @@
+#' Produce a dataframe with numbers and percentages
+#'
+#' @description Takes a data frame and tabulates two variables producing
+#'              the number and percentage either by row, column or total.
+#'
+#' @details
+#'
+#' People like to see the raw numbers but often think about them in
+#' percentages, this wrapper function facilitates production of such tables.
+#'
+#'
+#' @param df Data frame.
+#' @param id Unique identifier for individuals.
+#' @param select Variables to be summarised.
+#' @param group Variables by which to summarise the data by.
+#' @param digits Number of decimal places to be used in proportion/percentages.
+#'
+#' @export
+table_summary <- function(df     = .,
+                          id     = individual_id,
+                          select = c(),
+                          group  = site,
+                          digits = 3,
+                          ...){
+    ## Quote all arguments (see http://dplyr.tidyverse.org/articles/programming.html)
+    quo_id     <- enquo(id)
+    quo_select <- enquo(select)
+    quo_group  <- enquo(group)
+    ## Subset the data
+    df <- df %>%
+          dplyr::select(!!quo_id, !!quo_select, !!quo_group) %>%
+          unique()
+    ## gather() data, just in case there is > 1 variable selected to be summarised
+    df <- df %>%
+        gather(key = variable, value = value, !!quo_select)
+    ## Summarise selected variables by specified groups
+    results <- df %>%
+               group_by(!!quo_group, variable) %>%
+               summarise(n    = n(),
+                         mean = mean(value, na.rm = TRUE),
+                         sd   = sd(value, na.rm = TRUE),
+                         p25  = quantile(value, probs = 0.25, na.rm = TRUE),
+                         p50  = quantile(value, probs = 0.50, na.rm = TRUE),
+                         p70  = quantile(value, probs = 0.75, na.rm = TRUE),
+                         min  = min(value, na.rm = TRUE),
+                         max  = max(value, na.rm = TRUE))
+    return(results)
+}
