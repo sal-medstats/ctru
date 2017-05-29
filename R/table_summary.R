570d8d5038600f40d6531c06c54f7f62b0ee5dea
Neil Shephard <n.shephard@sheffield.ac.uk>
2017-05-29 09:09:50 +0100

    working version, not completely happy as grouping variables are specified as ... rather than explicitly

--- R/table_summary.R
+++ R/table_summary.R
@@ -19,30 +19,37 @@
 table_summary <- function(df     = .,
                           id     = individual_id,
                           select = c(),
-                          group  = site,
-                          digits = 3,
+                          ## group  = c(),
+                          ## digits = 3,
                           ...){
     ## Quote all arguments (see http://dplyr.tidyverse.org/articles/programming.html)
     quo_id     <- enquo(id)
     quo_select <- enquo(select)
-    quo_group  <- enquo(group)
+    ## quo_group  <- quos(group)
+    quo_group  <- quos(...)
+    UQS(quo_group) %>% print()
     ## Subset the data
     df <- df %>%
-          dplyr::select(!!quo_id, !!quo_select, !!quo_group) %>%
+          dplyr::select(!!quo_id, !!quo_select, !!!quo_group) %>%
           unique()
     ## gather() data, just in case there is > 1 variable selected to be summarised
     df <- df %>%
           gather(key = variable, value = value, !!quo_select)
     ## Summarise selected variables by specified groups
     results <- df %>%
-               group_by(!!quo_group, variable) %>%
-               summarise(n    = n(),
-                         mean = mean(value, na.rm = TRUE),
-                         sd   = sd(value, na.rm = TRUE),
-                         p25  = quantile(value, probs = 0.25, na.rm = TRUE),
-                         p50  = quantile(value, probs = 0.50, na.rm = TRUE),
-                         p70  = quantile(value, probs = 0.75, na.rm = TRUE),
-                         min  = min(value, na.rm = TRUE),
-                         max  = max(value, na.rm = TRUE))
+               group_by(!!!quo_group, variable) %>%
+               summarise(n       = n(),
+                         missing = sum(is.na(value)),
+                         mean    = mean(value, na.rm = TRUE),
+                         sd      = sd(value, na.rm = TRUE),
+                         p01     = quantile(value, probs = 0.01, na.rm = TRUE),
+                         p05     = quantile(value, probs = 0.05, na.rm = TRUE),
+                         p25     = quantile(value, probs = 0.25, na.rm = TRUE),
+                         p50     = quantile(value, probs = 0.50, na.rm = TRUE),
+                         p70     = quantile(value, probs = 0.75, na.rm = TRUE),
+                         p95     = quantile(value, probs = 0.95, na.rm = TRUE),
+                         p99     = quantile(value, probs = 0.99, na.rm = TRUE),
+                         min     = min(value, na.rm = TRUE),
+                         max     = max(value, na.rm = TRUE))
     return(results)
 }
