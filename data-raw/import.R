0f3f8b84113678951db0b984b4f9cef527faec30
Neil Shephard <n.shephard@sheffield.ac.uk>
2017-05-30 10:17:28 +0100

    Postcode/LSOA mapping

---
+++ data-raw/import.R
@@ -0,0 +1,8 @@
+## Import and save various source files
+##
+## Data Description : LSOA/Postcode
+## Location         : http://ons.maps.arcgis.com/home/item.html?id=ef72efd6adf64b11a2228f7b3e95deea
+lsoa_postcode_2011 <- read.csv('csv/PCD11_OA11_LSOA11_MSOA11_LAD11_EW_LU_aligned_v2.csv')
+names(lsoa_postcode_2011) <- names(lsoa_postcode_2011) %>% tolower()
+save(lsoa_postcode_2011,
+     file = '../data/lsoa_postcode.RData')
