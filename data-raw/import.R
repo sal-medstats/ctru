5ed955fe98fdfe530dc6e9acc42359fe21a6d2f7
Neil Shephard <n.shephard@sheffield.ac.uk>
2017-05-30 10:20:34 +0100

    converted lsoa names to match those used in the data.

--- data-raw/import.R
+++ data-raw/import.R
@@ -4,5 +4,7 @@
 ## Location         : http://ons.maps.arcgis.com/home/item.html?id=ef72efd6adf64b11a2228f7b3e95deea
 lsoa_postcode_2011 <- read.csv('csv/PCD11_OA11_LSOA11_MSOA11_LAD11_EW_LU_aligned_v2.csv')
 names(lsoa_postcode_2011) <- names(lsoa_postcode_2011) %>% tolower()
+names(lsoa_postcode_2011) <- gsub('lsoa11cd', 'lsoa_code_2011', names(lsoa_postcode_2011))
+names(lsoa_postcode_2011) <- gsub('lsoa11nm', 'lsoa_name_2011', names(lsoa_postcode_2011))
 save(lsoa_postcode_2011,
      file = '../data/lsoa_postcode.RData')
