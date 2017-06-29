9a0423f7bffd0d080b6b92795c55e2f10c04b7a4
Neil Shephard <n.shephard@sheffield.ac.uk>
2017-06-29 16:17:16 +0100

    Made plotting of numerical/factor conditional on there being variables specified that are either numeric or factorial (otherwise left_join() fails when merging with labels and function doesn't work)

--- R/plot_summary.R
+++ R/plot_summary.R
@@ -50,75 +50,81 @@ plot_summary <- function(df               = .,
     ## Since quo_events is used to control subsequent steps if its NULL select()
     ## fails, therefore conditionally build the variables that are select on
     to_select <- c(quo_id, quo_group)
-    if(!is.null(events)){
-        to_select <- c(to_select, quo_events)
-    }
+    ## if(!is.null(events)){
+    ##     to_select <- c(to_select, quo_events)
+    ## }
     ## Subset the data and de-duplicate
     df <- df %>%
           ## dplyr::select(!!quo_id, !!quo_select, !!quo_events, !!quo_group) %>%
           dplyr::select(!!!to_select, !!quo_select) %>%
           unique()
     ##########################################################################
-    ## Factor Variables                                                     ##
+    ## Continuous Variables                                                 ##
     ##########################################################################
     ## Subset the continuous variables gather() and bind with the lookup descriptions
     ## so that when plotted the graphs have meaningful titles
     numeric_vars <- which(sapply(df, class) == 'numeric') %>% names()
+    ## Logical check required to determine if numeric variables are to be plotted.
+    ## If none are specified then left_join() fails...
     results$df_numeric <- df %>%
                           dplyr::select(which(sapply(., class) == 'numeric'),
                                         !!!to_select) %>%
-                          gather(key = variable, value = value, numeric_vars) %>%
-                          left_join(.,
-                                    lookup,
-                                    by = c('variable' = 'identifier')) %>%
-                          ## Ensure value is numberic otherwise nothing to plot
-                          mutate(value = as.numeric(value))
-    ## Generate plot
-    results$continuous <- results$df_numeric %>%
-                          dplyr::filter(!is.na(!!quo_group)) %>%
-                          ggplot(aes_(~value, fill = quo_group)) +
-                          geom_histogram(position = position) +
-                          xlab('') + ylab('N') +
-                          ggtitle(title.continuous) +
-                          theme +
-                          theme(strip.background = element_blank(),
-                                strip.placement  = 'outside')
-    ## Facetted plot when no events specified
-    if(is.null(events)){
-        results$continuous <- results$continuous +
-                              facet_wrap(~label,
-                                         scales = 'free',
-                                         strip.position = 'bottom')
-    }
-    else{
-        results$continuous <- results$continuous +
-                              facet_grid(label~quo_events,
-                                         scales = 'free')
-    }
-    if(plotly == TRUE){
-        results$continuous <- results$continuous %>%
-                              ggplotly()
-    }
-    ## Plot individual figures if requested
-    if(individual == TRUE){
-        for(x in numeric_vars){
-            ## Extract the label
-            xlabel <- results$df_numeric %>%
-                      dplyr::filter(variable == x) %>%
-                      dplyr::select(label) %>%
-                      unique() %>%
-                      as.data.frame()
-            ## Plot current variable
-            results[[x]] <- results$df_numeric %>%
-                            dplyr::filter(!is.na(!!quo_group) & variable == x) %>%
-                            ggplot(aes_(~value, fill = quo_group)) +
-                            geom_histogram(position = position) +
-                            xlab(xlabel[[1]]) +
-                            ylab('N') +
-                            theme
-            if(plotly == TRUE){
-                results[[x]] <- results[[x]] %>%
-                                ggplotly()
+                          gather(key = variable, value = value, numeric_vars)
+    if(names(results$df_numeric) %in% c('variable')){
+        results$df_numeric <- results$df_numeric %>%
+                              left_join(.,
+                                        lookup,
+                                        by = c('variable' = 'identifier')) %>%
+                              ## Ensure value is numberic otherwise nothing to plot
+                              mutate(value = as.numeric(value))
+        print('Are we here?')
+        ## Generate plot
+        results$continuous <- results$df_numeric %>%
+                              dplyr::filter(!is.na(!!quo_group)) %>%
+                              ggplot(aes_(~value, fill = quo_group)) +
+                              geom_histogram(position = position) +
+                              xlab('') + ylab('N') +
+                              ggtitle(title.continuous) +
+                              theme +
+                              theme(strip.background = element_blank(),
+                                    strip.placement  = 'outside')
+        ## Facetted plot when no events specified
+        if(is.null(events)){
+            results$continuous <- results$continuous +
+                                  facet_wrap(~label,
+                                             scales = 'free',
+                                             strip.position = 'bottom')
+        }
+        else{
+            results$continuous <- results$continuous +
+                                  facet_grid(label~quo_events,
+                                             scales = 'free')
+        }
+        if(plotly == TRUE){
+            results$continuous <- results$continuous %>%
+                                  ggplotly()
+        }
+        ## Plot individual figures if requested
+        if(individual == TRUE){
+            for(x in numeric_vars){
+                ## Extract the label
+                xlabel <- results$df_numeric %>%
+                          dplyr::filter(variable == x) %>%
+                          dplyr::select(label) %>%
+                          unique() %>%
+                          as.data.frame()
+                ## Plot current variable
+                results[[x]] <- results$df_numeric %>%
+                                dplyr::filter(!is.na(!!quo_group) & variable == x) %>%
+                                ggplot(aes_(~value, fill = quo_group)) +
+                                geom_histogram(position = position) +
+                                xlab(xlabel[[1]]) +
+                                ylab('N') +
+                                theme
+                if(plotly == TRUE){
+                    results[[x]] <- results[[x]] %>%
+                                    ggplotly()
+                }
             }
         }
     }
@@ -131,81 +137,85 @@ plot_summary <- function(df               = .,
     ##########################################################################
     ## Subset factor variables, gather() and plot these
     factor_vars <- which(sapply(df, class) == 'factor') %>% names()
+    ## Logical check required to determine if numeric variables are to be plotted.
+    ## If none are specified then left_join() fails...
     results$df_factor <- df %>%
                          dplyr::select(which(sapply(., class) == 'factor'),
                                        !!!to_select) %>%
-                         gather(key = variable, value = value, factor_vars) %>%
-                         left_join(.,
-                                   lookup,
-                                   by = c('variable' = 'identifier'))
-    ## Remove NAs
-    if(remove.na == TRUE){
+                         gather(key = variable, value = value, factor_vars)
+    if(names(results$df_numeric) %in% c('variable')){
         results$df_factor <- results$df_factor %>%
-                             dplyr::filter(!is.na(value))
-    }
-    ## Plot
-    ## ToDo : group factor variables based on the form, plot each as 1 x group
-    ##        then use gridExtra() to arrange.
-    factor_sets <- results$df_factor %>%
-                   dplyr::select(form) %>%
-                   table() %>%
-                   names()
-    ## Plot groups of factors based on the Form they are collected on
-    results_length_pre <- length(results)
-    for(x in factor_sets){
-        out <- gsub(' ', '_', x) %>%
-               gsub('\\(', '', .) %>%
-               gsub('\\)', '', .) %>%
-               gsub('-', '_', .) %>%
-               tolower()
-        results[[paste0('factor_', out)]] <- results$df_factor %>%
-                                             dplyr::filter(form == x) %>%
-                                             ggplot(aes(x = label, fill = value),
-                                                    position = position_stack(reverse = TRUE)) +
-                                             geom_bar(position = 'fill') +
-                                             coord_flip() +
-                                             xlab('') + ylab('Proportion') +
-                                             ggtitle(x) +
-                                             facet_grid(form~group,
-                                                        scales = 'free') +
-                                             theme +
-                                             theme(strip.background = element_blank(),
-                                                   strip.placement  = 'outside')
-    }
-    results_length_post <- length(results)
-
-    results$factor <- results$df_factor %>%
-                      ggplot(aes(x = label, fill = value),
-                                position = position_stack(reverse = TRUE)) +
-                      geom_bar(position = 'fill') +
-                      coord_flip() +
-                      xlab('') + ylab('Proportion') +
-                      ggtitle(title.factor) +
-                      facet_grid(form~group,
-                                 scales = 'free') +
-                      theme +
-                      theme(strip.background = element_blank(),
-                            strip.placement  = 'outside')
-    if(individual == TRUE){
-        for(x in factor_vars){
-            ## Extract the label
-            xlabel <- results$df_factor %>%
-                      dplyr::filter(variable == x) %>%
-                      dplyr::select(label) %>%
-                      unique() %>%
-                      as.data.frame()
-            ## Plot current variable
-            results[[x]] <- results$df_factor %>%
-                            dplyr::filter(!is.na(!!quo_group) & variable == x) %>%
-                            ggplot(aes(x = label, fill = value)) +
-                            geom_bar(position = 'fill') +
-                            coord_flip() +
-                            xlab(xlabel[[1]]) +
-                            ylab('N') +
-                            theme
-            if(plotly == TRUE){
-                results[[x]] <- results[[x]] %>%
-                                ggplotly()
+                             left_join(.,
+                                       lookup,
+                                       by = c('variable' = 'identifier'))
+        ## Remove NAs
+        if(remove.na == TRUE){
+            results$df_factor <- results$df_factor %>%
+                                 dplyr::filter(!is.na(value))
+        }
+        ## Plot
+        ## ToDo : group factor variables based on the form, plot each as 1 x group
+        ##        then use gridExtra() to arrange.
+        factor_sets <- results$df_factor %>%
+                       dplyr::select(form) %>%
+                       table() %>%
+                       names()
+        ## Plot groups of factors based on the Form they are collected on
+        results_length_pre <- length(results)
+        for(x in factor_sets){
+            out <- gsub(' ', '_', x) %>%
+                   gsub('\\(', '', .) %>%
+                   gsub('\\)', '', .) %>%
+                   gsub('-', '_', .) %>%
+                   tolower()
+            results[[paste0('factor_', out)]] <- results$df_factor %>%
+                                                 dplyr::filter(form == x) %>%
+                                                 ggplot(aes(x = label, fill = value),
+                                                        position = position_stack(reverse = TRUE)) +
+                                                 geom_bar(position = 'fill') +
+                                                 coord_flip() +
+                                                 xlab('') + ylab('Proportion') +
+                                                 ggtitle(x) +
+                                                 facet_grid(form~group,
+                                                            scales = 'free') +
+                                                 theme +
+                                                 theme(strip.background = element_blank(),
+                                                       strip.placement  = 'outside')
+        }
+        results_length_post <- length(results)
+        results$factor <- results$df_factor %>%
+                          ggplot(aes(x = label, fill = value),
+                                 position = position_stack(reverse = TRUE)) +
+                          geom_bar(position = 'fill') +
+                          coord_flip() +
+                          xlab('') + ylab('Proportion') +
+                          ggtitle(title.factor) +
+                          facet_grid(form~group,
+                                     scales = 'free') +
+                          theme +
+                          theme(strip.background = element_blank(),
+                                strip.placement  = 'outside')
+        if(individual == TRUE){
+            for(x in factor_vars){
+                ## Extract the label
+                xlabel <- results$df_factor %>%
+                          dplyr::filter(variable == x) %>%
+                          dplyr::select(label) %>%
+                          unique() %>%
+                          as.data.frame()
+                ## Plot current variable
+                results[[x]] <- results$df_factor %>%
+                                dplyr::filter(!is.na(!!quo_group) & variable == x) %>%
+                                ggplot(aes(x = label, fill = value)) +
+                                geom_bar(position = 'fill') +
+                                coord_flip() +
+                                xlab(xlabel[[1]]) +
+                                ylab('N') +
+                                theme
+                if(plotly == TRUE){
+                    results[[x]] <- results[[x]] %>%
+                                    ggplotly()
+                }
             }
         }
     }
