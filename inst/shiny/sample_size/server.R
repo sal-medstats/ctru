99e05c3f202acba959af15eb01502b5eac009cf4
slackline <nshephard@gmail.com>
2018-02-22 09:28:52 +0000

    skeleton

---
+++ inst/shiny/sample_size/server.R
@@ -0,0 +1,10 @@
+server <- function(input, output, session){
+    ## pwr calculations
+    output$pwr <- reactive({
+
+    })
+    ## clusterPower calculations
+    output$clusterPower <- reactive({
+
+    })
+}
