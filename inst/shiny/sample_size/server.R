6ed3a2df6c62e522afba68453b0e8841ddc0d62b
slackline <nshephard@gmail.com>
2018-02-22 12:45:35 +0000

    options for pwr

--- inst/shiny/sample_size/server.R
+++ inst/shiny/sample_size/server.R
@@ -1,7 +1,30 @@
 server <- function(input, output, session){
     ## pwr calculations
     output$pwr <- reactive({
+        if(input$test == "pwr.p.test"){
 
+        }
+        else if(input$test == "pwr.2p.test"){
+
+        }
+        else if(input$test == "pwr.2pn.test"){
+
+        }
+        else if(input$test == "pwr.t.test"){
+
+        }
+        else if(input$test == "pwr.anova.test"){
+
+        }
+        else if(input$test == "pwr.r.test"){
+
+        }
+        else if(input$test == "pwr.chisq.test"){
+
+        }
+        else if(input$test == "pwr.f2.test"){
+
+        }
     })
     ## clusterPower calculations
     output$clusterPower <- reactive({
