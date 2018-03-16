2b964a267ca3e72a34d5ff494a4d810150e265f4
slackline <nshephard@gmail.com>
2018-03-16 11:30:19 +0000

    Added valueBoxOutput for pwr results

--- inst/shiny/sample_size/server.R
+++ inst/shiny/sample_size/server.R
@@ -1,6 +1,6 @@
 server <- function(input, output, session){
     ## pwr calculations
-    output$pwr <- reactive({
+    pwr_results <- reactive({
         if(input$test == "pwr.p.test"){
             results <- pwr::pwr.p.test(h           = input$h,
                                        power       = input$power,
@@ -52,11 +52,52 @@ server <- function(input, output, session){
                                            sig.level   = input$sig.level,
                                            alternative = input$alternative)
         }
-        ceiling(results$n)
+        results
+    })
+    ## ValueBox : pwr n
+    output$pwr_n <- renderValueBox({
+        valueBox(value    = formatC(ceiling(pwr_results()$n)),
+                 subtitle = "Number of participants (per arm)",
+                 icon     = icon("users"),
+                 width    = 12,
+                 color    = "blue")
+    })
+    ## ValueBox : pwr alpha
+    output$pwr_alpha <- renderValueBox({
+        valueBox(value    = formatC(pwr_results()$sig.level,
+                                    format = "f",
+                                    digits = 4),
+                 subtitle = "Significance Level",
+                 icon     = icon("users"),
+                 width    = 12,
+                 color    = "green")
+    })
+    ## ValueBox : pwr power
+    output$pwr_power <- renderValueBox({
+        valueBox(value    = formatC(pwr_results()$power,
+                                    format = "f",
+                                    digits = 4),
+                 subtitle = "Power",
+                 icon     = icon("users"),
+                 color    = "cyan")
     })
     ## samplesize calculations
     output$samplesize <- reactive({
-
+        if(input$test == "n.ttest"){
+            results <- samplesize:n.ttest(power     = input$power,
+                                          alpha     = input$alpha,
+                                          mead.diff = input$mean.diff,
+                                          sd1       = input$sd1,
+                                          sd2       = input$sd2,
+                                          k         = input$k,
+                                          design    = input$design,
+                                          fraction  = input$fraction,
+                                          variance  = input$variance)
+        }
+        else if(input$test == "n.wilcox.ord"){
+            results <- samplesize:n.wilcox.ord()
+        }
+        results
     })
     ## TrialSize calculations
     output$TrialSize <- reactive({
